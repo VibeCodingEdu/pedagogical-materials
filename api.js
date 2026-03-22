f60810ddb22972e3661ff969f8fc10960682ea73
Mendy <anochinstitute@gmail.com>
2026-03-22 23:45:19 +0200

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@
  *  API — JSONP (zero CORS issues)
  * ═══════════════════════════════════════════
  */
-const API_URL = 'https://script.google.com/macros/s/AKfycbz3GJGG05Ov1W4Zz-wjt2zKUHD1us6s-fuoREd-tKB2DXzGjpVNoyFevbZWTU1ruC0/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbym_8RUM2Fiuvwkj9zXCqy0lUW1cQjUE4TUMJzHUMD_T-lIFcTY3YfmTz_npmOj-uU/exec';
 
 const ADMIN_URL = API_URL + (API_URL.includes('?') ? '&' : '?') + 'page=admin';
 
